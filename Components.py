a13012fd46c5aecb12e7c5a8d94acd4dfe8717e9
Christopher Jorge <Christopherjorge47@icloud.com>
2025-01-29 12:09:16 -0500

    Add clarifying comments to components.py

--- Components.py
+++ Components.py
@@ -1,34 +1,34 @@
 import pygame as game
 
-game.font.init()
+game.font.init() # Initialize the pyfame font function, used to put text in buttons
 
 class Button():
 
-    def __init__(self, screen, colorRGB, height, width, xCoordinate, yCoordinate):
-        self.xCoordinate = xCoordinate
-        self.yCoordinate = yCoordinate
-        self.height = height
-        self.width = width 
-        self.screen = screen
-        self.color = colorRGB
-        self.rectangle =  game.Rect(self.xCoordinate, self.yCoordinate, self.width, self.height)
-        self.clicked = False
-        game.draw.rect(self.screen, self.color, self.rectangle)
+    def __init__(self, screen, colorRGB, height, width, xCoordinate, yCoordinate): # Function to initialize the button class
+        self.xCoordinate = xCoordinate # Initialize xCoordinate, used to place button on screen 
+        self.yCoordinate = yCoordinate # Initialize yCoordinate, used to place button on screen
+        self.height = height # Initialize height, used to determine button height
+        self.width = width  # Initilaize width, used to determine button width
+        self.screen = screen # Initialize the screen, used to put the buttons on the screen
+        self.color = colorRGB # Initialize color, used to determine the buttons color
+        self.rectangle =  game.Rect(self.xCoordinate, self.yCoordinate, self.width, self.height) # Create the button
+        self.clicked = False # Initialize the buttons clicked value, used to determine if button has been clicked
+        game.draw.rect(self.screen, self.color, self.rectangle) # Draw the button onto the screen
 
-    def write(self, text, size):
-        font = game.font.SysFont('Calibri', size, True, False)
-        self.text = font.render(text,True, (0,0,0))
-        self.middleWidth = self.width // 2
-        self.middleHeight = self.height // 2
+    def write(self, text, size): # Function to add text to the button
+        font = game.font.SysFont('Calibri', size, True, False) # Initialize the type of font
+        self.text = font.render(text,True, (0,0,0)) # Render the text
+        self.middleWidth = self.width // 2 # Get the middle of the button width wise
+        self.middleHeight = self.height // 2 # Get the middle of the button height wise
+        # place the text at the center of the button
         self.screen.blit(self.text, [(self.xCoordinate - (self.text.get_width() // 2)) + self.middleWidth, self.yCoordinate - (self.text.get_height() // 2) + self.middleHeight])
 
 
-    def action(self, func):
-        mousePosition = game.mouse.get_pos()
-
+    def action(self, func): # Function to add functionality to the button
+        mousePosition = game.mouse.get_pos() # Get the mouse position on the screen
         if self.rectangle.collidepoint(mousePosition): # Check if mouse cursor is colliding with the button
             if game.mouse.get_pressed()[0] == 1 and self.clicked == False: # Check if button has been right clicked and the button has not been clicked yet
-                self.clicked = True
-                func(self.screen)
-            if game.mouse.get_pressed()[0] == 0:
-                self.clicked = False
+                self.clicked = True # Set self.clicked to true
+                func(self.screen) # Call the function linked to the button
+            if game.mouse.get_pressed()[0] == 0: # Check if button is no longer being clicked
+                self.clicked = False # Set self.clicked to false
